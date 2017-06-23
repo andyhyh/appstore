ef04acff7e44e6853ee433832e4bbebdcd2e908a
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-06-23 14:44:13 +0200

    Update the route of one of the handlers to reflect the fact that only one package is fetched.

--- cmd/appstore-server/routes.go
+++ cmd/appstore-server/routes.go
@@ -49,7 +49,7 @@ func createDashboardRouter(settings *helm_env.EnvSettings, templates map[string]
 	// TODO: Reuse the createPackageRouter instead, and instead specify a custom writer, or something.
 	baseDashboardRouter.Route("/", func(baseDashboardRouter chi.Router) {
 		baseDashboardRouter.Get("/", makePackageIndexHandler(settings, templates))
-		baseDashboardRouter.Get("/packages/:packageName", makePackageDetailHandler(settings, templates))
+		baseDashboardRouter.Get("/package/:packageName", makePackageDetailHandler(settings, templates))
 		baseDashboardRouter.Get("/releases", makeReleaseOverviewHandle(settings, templates))
 	})
 
