2ff84216ef0ab82499517e27e9c0217627ddad07
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-06-23 22:30:08 +0200

    Stop using the helper function to parse JSON, as the handlers return different results.

--- cmd/appstore-server/api_endpoint_handlers_test.go
+++ cmd/appstore-server/api_endpoint_handlers_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"github.com/pressly/chi"
 	"k8s.io/helm/cmd/helm/search"
@@ -9,18 +8,14 @@ import (
 	"testing"
 )
 
-func checkValidJSONRes(body *bytes.Buffer, t *testing.T) {
-	var results []*search.Result
+func TestPackageIndexHandler(t *testing.T) {
+	resp, body := testHandler(t, makeListAllPackagesHandler(mockSettings), "GET", "/", nil)
+	checkStatus(resp, http.StatusOK, t)
+	var results [][]*search.Result
 	err := json.NewDecoder(body).Decode(&results)
 	if err != nil {
 		t.Errorf("decoding of result failed: %s", err.Error())
 	}
-}
-
-func TestPackageIndexHandler(t *testing.T) {
-	resp, body := testHandler(t, makeListAllPackagesHandler(mockSettings), "GET", "/", nil)
-	checkStatus(resp, http.StatusOK, t)
-	checkValidJSONRes(body, t)
 }
 
 func TestPackageSearchHandler(t *testing.T) {
@@ -29,5 +24,9 @@ func TestPackageSearchHandler(t *testing.T) {
 
 	resp, body := testHandler(t, r, "GET", "/test", nil)
 	checkStatus(resp, http.StatusOK, t)
-	checkValidJSONRes(body, t)
+	var results []*search.Result
+	err := json.NewDecoder(body).Decode(&results)
+	if err != nil {
+		t.Errorf("decoding of result failed: %s", err.Error())
+	}
 }
