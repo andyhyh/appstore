c82defd577d6bd172d362a0f746cc4af49c0b14a
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-06-25 12:13:56 +0200

    Remove a bunch of code that is no longer needed.

--- pkg/templateutil/render_utils.go
+++ pkg/templateutil/render_utils.go
@@ -1,38 +1,11 @@
 package templateutil
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
 	"path"
 	"path/filepath"
 )
-
-func isMap(item interface{}) bool {
-	switch item.(type) {
-	case map[string]interface{}:
-		return true
-	default:
-		return false
-	}
-}
-
-func getType(item interface{}) string {
-	switch v := item.(type) {
-	case string:
-		return "string"
-	case bool:
-		return "bool"
-	case int, float64, float32:
-		return "int"
-	case uint:
-		return "uint"
-	case []interface{}:
-		return "list"
-	default:
-		return fmt.Sprintf("%T", v)
-	}
-}
 
 func ProcessTemplates(templatesDir string) (map[string]*template.Template, error) {
 	layouts, err := filepath.Glob(path.Join(templatesDir, "layouts/*.html"))
@@ -45,12 +18,10 @@ func ProcessTemplates(templatesDir string) (map[string]*template.Template, error
 		return nil, err
 	}
 
-	funcMap := template.FuncMap{"isMap": isMap, "getType": getType}
-
 	processedTemplates := make(map[string]*template.Template)
 	for _, layout := range layouts {
 		files := append(bases, layout)
-		tmpl := template.Must(template.New(layout).Funcs(funcMap).ParseFiles(files...))
+		tmpl := template.Must(template.ParseFiles(files...))
 		processedTemplates[filepath.Base(layout)] = tmpl
 	}
 
