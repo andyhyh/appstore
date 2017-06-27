62dabf97cb29ece301a1a9f365d7b31623235335
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-06-27 13:44:56 +0200

    Ensure that all helm related config files are created.

--- cmd/appstore-server/main.go
+++ cmd/appstore-server/main.go
@@ -19,9 +19,24 @@ import (
 	log "github.com/Sirupsen/logrus"
 
 	helm_env "k8s.io/helm/pkg/helm/environment"
+	"k8s.io/helm/pkg/helm/helmpath"
+	"k8s.io/helm/pkg/getter"
+	"k8s.io/helm/pkg/repo"
+)
+
+const (
+	stableRepository         = "stable"
+	localRepository          = "local"
+	localRepositoryIndexFile = "index.yaml"
 )
 
 var startTime time.Time
+var (
+	stableRepositoryURL = "https://kubernetes-charts.storage.googleapis.com"
+	// This is the IPv4 loopback, not localhost, because we have to force IPv4
+	// for Dockerized Helm: https://github.com/kubernetes/helm/issues/1410
+	localRepositoryURL = "http://127.0.0.1:8879/charts"
+)
 
 const version string = "v1"
 
@@ -32,6 +47,16 @@ func main() {
 	flag.Parse()
 
 	settings := helmutil.InitHelmSettings(*debug, *tillerHost)
+
+	if err := ensureDirectories(settings.Home); err != nil {
+		panic(err)
+	}
+	if err := ensureDefaultRepos(settings.Home, settings, false); err != nil {
+		panic(err)
+	}
+	if err := ensureRepoFileFormat(settings.Home.RepositoryFile()); err != nil {
+		panic(err)
+	}
 
 	baseRouter := chi.NewRouter()
 
@@ -56,4 +81,108 @@ func main() {
 	log.Debug("Tiller host: ", settings.TillerHost)
 	startTime = time.Now()
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), baseRouter))
+}
+
+func ensureDirectories(home helmpath.Home) error {
+	configDirectories := []string{
+		home.String(),
+		home.Repository(),
+		home.Cache(),
+		home.LocalRepository(),
+		home.Plugins(),
+		home.Starters(),
+		home.Archive(),
+	}
+	for _, p := range configDirectories {
+		if fi, err := os.Stat(p); err != nil {
+			fmt.Printf("Creating %s \n", p)
+			if err := os.MkdirAll(p, 0755); err != nil {
+				return fmt.Errorf("Could not create %s: %s", p, err)
+			}
+		} else if !fi.IsDir() {
+			return fmt.Errorf("%s must be a directory", p)
+		}
+	}
+
+	return nil
+}
+
+func ensureDefaultRepos(home helmpath.Home, settings *helm_env.EnvSettings, skipRefresh bool) error {
+	repoFile := home.RepositoryFile()
+	if fi, err := os.Stat(repoFile); err != nil {
+		fmt.Printf("Creating %s \n", repoFile)
+		f := repo.NewRepoFile()
+		sr, err := initStableRepo(home.CacheIndex(stableRepository), settings, skipRefresh)
+		if err != nil {
+			return err
+		}
+		lr, err := initLocalRepo(home.LocalRepository(localRepositoryIndexFile), home.CacheIndex("local"))
+		if err != nil {
+			return err
+		}
+		f.Add(sr)
+		f.Add(lr)
+		if err := f.WriteFile(repoFile, 0644); err != nil {
+			return err
+		}
+	} else if fi.IsDir() {
+		return fmt.Errorf("%s must be a file, not a directory", repoFile)
+	}
+	return nil
+}
+
+func initStableRepo(cacheFile string, settings *helm_env.EnvSettings, skipRefresh bool) (*repo.Entry, error) {
+	c := repo.Entry{
+		Name:  stableRepository,
+		URL:   stableRepositoryURL,
+		Cache: cacheFile,
+	}
+	r, err := repo.NewChartRepository(&c, getter.All(*settings))
+	if err != nil {
+		return nil, err
+	}
+
+	if skipRefresh {
+		return &c, nil
+	}
+
+	// In this case, the cacheFile is always absolute. So passing empty string
+	// is safe.
+	if err := r.DownloadIndexFile(""); err != nil {
+		return nil, fmt.Errorf("Looks like %q is not a valid chart repository or cannot be reached: %s", stableRepositoryURL, err.Error())
+	}
+
+	return &c, nil
+}
+
+func initLocalRepo(indexFile, cacheFile string) (*repo.Entry, error) {
+	if fi, err := os.Stat(indexFile); err != nil {
+		i := repo.NewIndexFile()
+		if err := i.WriteFile(indexFile, 0644); err != nil {
+			return nil, err
+		}
+
+		//TODO: take this out and replace with helm update functionality
+		os.Symlink(indexFile, cacheFile)
+	} else if fi.IsDir() {
+		return nil, fmt.Errorf("%s must be a file, not a directory", indexFile)
+	}
+
+	return &repo.Entry{
+		Name:  localRepository,
+		URL:   localRepositoryURL,
+		Cache: cacheFile,
+	}, nil
+}
+
+func ensureRepoFileFormat(file string) error {
+	r, err := repo.LoadRepositoriesFile(file)
+	if err == repo.ErrRepoOutOfDate {
+		fmt.Printf("Updating repository file format...")
+		if err := r.WriteFile(file, 0644); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
