d89312392f957d7748b5b580a7b96ade0907882c
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-06-29 12:54:59 +0200

    Convert the Travis repo slug to lowercase, as Docker does not like uppercase names.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 TAG=$1
 
-docker build -t quay.io/$TRAVIS_REPO_SLUG:$TAG .
+docker build -t quay.io/${TRAVIS_REPO_SLUG,,}:$TAG .
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" quay.io
-docker push quay.io/$TRAVIS_REPO_SLUG:$TAG
+docker push quay.io/${TRAVIS_REPO_SLUG,,}:$TAG
