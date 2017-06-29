efc8a5f95794c36e2285c6b7cb10204df12330e7
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-06-29 13:03:50 +0200

    Hardcode the URL to the Quay repo, as the repo does not have the same name in quay.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 set -euo pipefail
 TAG=$1
+QUAY_REPO=quay.io/uninett/k8s-appstore-backend
 
-docker build -t quay.io/${TRAVIS_REPO_SLUG,,}:$TAG .
+docker build -t $QUAY_REPO:$TAG .
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" quay.io
-docker push quay.io/${TRAVIS_REPO_SLUG,,}:$TAG
+docker push $QUAY_REPO:$TAG
