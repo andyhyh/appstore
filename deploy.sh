d981a2c1ac035861dd21059930e694d3dabd1c7d
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-06-29 12:27:03 +0200

    Ensure that the deploy script is executable.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 TAG=$1
 
-echo "Building image with Tag: $TAG"
 docker build -t quay.io/$TRAVIS_REPO_SLUG:$TAG .
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" quay.io
 docker push quay.io/$TRAVIS_REPO_SLUG:$TAG
